83d41587e2c788c2f37c9cd94a02d887342fedd7
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-07 01:30:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - A program that prints some given input followed by a new line.
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n", stdout);
+
+	return (0);
+}
