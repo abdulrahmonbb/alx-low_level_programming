21f7a11d93406b21c1a5776669ef56dd5d610a01
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-07 02:50:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: (1)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
