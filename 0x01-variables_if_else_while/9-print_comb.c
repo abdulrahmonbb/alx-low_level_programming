0580e455404ce98d1ba006a461c9756dd59a19cf
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-09 02:09:09 -0700

    9pc

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,15 @@
 
 int main(void)
 {
-	int x, y;
+	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9; y++)
-		{
 			putchar('0' + x);
 			putchar(',');
 			putchar(' ');
-			putchar('0' + y);
-		}
 	}
+
+	putchar('\n');
 	return (0);
 }
