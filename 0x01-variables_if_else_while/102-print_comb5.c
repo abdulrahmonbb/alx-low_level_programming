2e431d933a0f4ec5138f8af08f622cb9d1787627
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-11 02:47:12 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,15 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 99; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = 0; num2 < 100; num2++)
+		for (num2 = 0; num2 <= 99; num2++)
 		{
-			putchar('0' + (num1 / 10));
-			putchar('0' + (num2 % 10));
+			putchar((num1 / 10) + '0');
+			putchar((num2 % 10) + '0');
 			putchar(' ');
-			putchar('0' + (num2 / 10));
-			putchar('0' + (num2 % 10));
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 99)
 				continue;
