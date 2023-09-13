b750b1d04fc4bd56abdea80e3e474b7301ebf935
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-13 07:09:51 -0700

    99

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,28 +17,25 @@ void times_table(void)
 		{
 			product = i * j;
 
-			if (product > 9 && j != 9)
+			if (j == 0)
+			{
+				_putchar(product + '0');
+			}
+			else if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+			else
 			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
+			}
+
+			if (j < 9)
+			{
 				_putchar(',');
 				_putchar(' ');
-			}
-			else if (product > 9 && j == 9)
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
-			else if (product < 10 && j != 9)
-			{
-				_putchar(product + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (product < 10 && j == 9)
-			{
-				_putchar(product + '0');
 			}
 			j++;
 		}
