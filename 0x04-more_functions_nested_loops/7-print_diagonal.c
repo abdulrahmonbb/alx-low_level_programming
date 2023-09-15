223d9cf0348b4ebb020d8c9a1a6ba09b2be3ac66
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-14 17:40:18 -0700

    7-diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,23 +9,19 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < n; i++)
 	{
 		if (n != 0 && n > 0)
 		{
-			if (n > 1)
+			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
-				_putchar('\\');
-				_putchar('\n');
 			}
-			else
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
+			
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
