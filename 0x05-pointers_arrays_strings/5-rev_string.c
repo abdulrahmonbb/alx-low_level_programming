1264d0d1d09dbca589791bee5da47159ff05feca
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-18 14:28:55 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: character string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = 0, i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
