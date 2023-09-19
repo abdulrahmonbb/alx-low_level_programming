b6887224f91fc9cc7a4b1632b067dc4f79a914ea
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-19 03:28:59 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,18 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		if (i != (n - 1))
+		if (i == (n - 1))
+		{
+			printf("%d", a[i]);
+		}
+		else
 		{
 			printf("%d, ", a[i]);
 		}
-		else
-			printf("%d", a[i]);
+		i++;
 	}
 }
