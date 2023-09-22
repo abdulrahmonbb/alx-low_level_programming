6a40ce4277620605e27536e04ee7cf45907f5217
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-22 00:33:34 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,19 +10,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *org = dest;
+	int i;
 
-	while (*src != '\0' && n > 0)
-	{
-		*dest++ = *src++;
-		n--;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
-	while (n > 0)
-	{
-		*dest = '\0';
-		n--;
-	}
-
-	return (org);
+	return (dest);
 }
