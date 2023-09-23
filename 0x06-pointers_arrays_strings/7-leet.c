f43d7007fb085b6225041472588dceb9da5459e1
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-23 01:06:13 -0700

    7-1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: string
+ * Return: encoded string
+ */
+char *leet(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+	}
+	return (str);
+}
+
