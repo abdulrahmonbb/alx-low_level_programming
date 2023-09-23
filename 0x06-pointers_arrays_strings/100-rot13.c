2f1817e7cbfb832ce82a69db99e7eb1318bb91ff
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-23 02:19:38 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string
+ *
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJnopqrstuvwxyzabcdefghij";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			if (str[i] == s1[i])
+			{
+				str[i] = s2[i];
+				break;
+			}
+		}
+	}
+	return (str);
+}
