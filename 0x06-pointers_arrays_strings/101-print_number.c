fabbbdd25a239235058e59043f93c64ecd6791ed
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-23 08:54:47 -0700

    101-print_numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,57 @@
  */
 void print_number(int n)
 {
-	if (n < 9)
+	if ( n == 0)
 	{
-		_putchar(n);
+		_putchar(48);
 	}
-	else if (n > 9)
+	else
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+		if (n < 0)
+		{
+			n *= -1;
+
+			if (n > 99)
+			{
+				_putchar('-');
+				_putchar((n / 100) + '0');
+				_putchar((n % 100) / 10 + '0');
+				_putchar((n % 100) % 10 + '0');
+			}
+			else if (n < 100 && n > 9)
+			{
+				_putchar('-');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else if (n < 9)
+			{
+				_putchar('-');
+				_putchar(n);
+			}
+		}
+		else if (n < 10 && n != 0)
+		{
+			
+			_putchar(n);
+		}
+		else if (n > 10 && n < 100)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+		else if (n > 100 && n < 1000)
+		{
+			_putchar((n / 100) + '0');
+			_putchar((n % 100) / 10 + '0');
+			_putchar((n % 100) % 10 + '0');
+		}
+		else if (n > 1000)
+		{
+			_putchar((n / 1000) + '0');
+			_putchar((n % 1000) / 100 + '0');
+			_putchar((n % 1000) / 10 + '0');
+			_putchar((n % 1000) % 10 + '0');
+		}
+	}	
 }
