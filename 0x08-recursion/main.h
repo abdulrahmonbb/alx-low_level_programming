40d08c9b2cde4be7bea4c058ce43c204a84b6116
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-09-29 07:43:06 -0700

    wildcmp

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,6 @@ int is_prime_number(int n);
 int _is_prime(int a, int b);
 int is_palindrome(char *s);
 int is_palindrome_recursive(char *s, int left, int right);
+int wildcmp(char *s1, char *s2);
 
 #endif
