522bd976ddc75837fa16ffecf35435876bed99ac
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-10-02 09:19:10 -0700

    1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,11 +11,15 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (*argv != NULL)
+	if (argc == 1)
 	{
-		for (i = 0; i < argc; i++)
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		for (i = 0; *argv; i++,argv++)
 			;
-		printf("%d\n", i);
+		printf("%d\n", i - 1);
 	}
 	return (0);
 }
