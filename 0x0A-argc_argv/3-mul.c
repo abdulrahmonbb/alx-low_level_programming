40e009226ce1692ca3edf80e0cb98be3cb71cfb6
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-10-02 14:20:33 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,18 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 1;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			sum *= atoi(argv[i]);
-		}
-	}
-	else
+	if (argc == 1 || argc == 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", sum);
+	else
+	{
+		for (i = 1; i < 3; i++)
+		{
+			sum *= atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
 	return (0);
 }
