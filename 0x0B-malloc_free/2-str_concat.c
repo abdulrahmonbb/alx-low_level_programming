9122e9ade391cb0576470fbec011f6600dd80e91
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-10-04 02:54:25 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,13 +22,18 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	
+
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 
 	result = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	
 	strcpy(result, s1);
 	strcat(result, s2);
-
 	return (result);
 }
