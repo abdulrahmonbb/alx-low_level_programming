fc0d77fdc70ed485e31cd49e746c40eae1be5148
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-10-10 14:54:49 -0700

    2 print dog v2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	printf("Name: %s\n", d->name != NULL ? d->name : "Name: (nil)");
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner != NULL ? d->owner : "Owner: (nil)");
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 
 }
