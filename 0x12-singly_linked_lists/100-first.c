bdb5293e6d2014ee8ae577f145c682eab2b9c3d2
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-10-21 09:42:17 -0700

    100 first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * start_up - executes before main
+ *
+ * Return: void
+ */
+void start_up(void)
+{
+	printf(" You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
