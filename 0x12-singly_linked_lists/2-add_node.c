bd69f1517f1cee2a96f5b3d0e5b776465860e5f4
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-10-20 21:52:22 -0700

    3 node add end

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: pointer to pointer to list_t
+ * @str: string
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *str_dup;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	
+	str_dup = strdup(str);
+	if (str_dup == NULL)
+	{
+		free (new_node);
+		return (NULL);
+	}
+	
+	new_node->str = str_dup;
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end -  adds a new node at the end of a list_t list.
+ * @head: pointer to pointer to list_t
+ * @str: string
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *current;
+	char *str_dup;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	str_dup = strdup(str);
+	if (str_dup == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = str_dup;
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if(*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+
+	return (new_node);
+}
