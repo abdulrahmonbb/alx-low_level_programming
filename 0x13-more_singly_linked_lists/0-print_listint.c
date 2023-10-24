7d00650e58717b009c3d4201291b94a50a9377f3
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-10-24 07:35:18 +0100

    0 print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
+	int i;
 	
 	for (i = 0; h; i++)
 	{
-		printf("%d\n", h->n)
+		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
