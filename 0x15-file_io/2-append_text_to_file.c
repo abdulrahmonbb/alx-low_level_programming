e13177e4755ec869361906290f497fa1dda80afa
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-11-29 06:11:33 -0800

    2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,17 +7,22 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, length;
+	int fd, res_write;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	length = strlen(text_content);
 	if (text_content != NULL)
 	{
-		write(fd, text_content, length);
+		res_write = write(fd, text_content, strlen(text_content));
+		if (res_write == -1)
+		{
+			return (-1);
+			close (fd);
+		}
+		
 	}
 	close (fd);
 	return (1);
