0c4be182ad3042040dc4b58a2af2a9b1af57791f
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-12-01 03:51:45 -0800

    reimplemented function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,29 @@
 #include "main.h"
 /**
  * create_file - creates a file
- * @filename: file string name
- * @text_content: string to write to the file
- * Return: 1 on success -1 on failure
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len;
-	ssize_t write_count;
+	int fd, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
-	len = strlen(text_content);
-	write_count = write(fd, text_content, len);
-	if (write_count == -1 || (int) write_count != len)
-	{
-		close(fd);
-		return (-1);
-	}
-	close(fd);
+	write(fd, text_content, i);
 	return (1);
 }
