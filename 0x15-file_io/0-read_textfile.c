413884202645ad2e025ed0055673678414c8af25
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-12-01 03:32:31 -0800

    fixed betty doc errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,8 +2,8 @@
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard
  * output.
- * filename: filename
- * letters: the number of letters it should read and print
+ * @filename: filename
+ * @letters: the number of letters it should read and print
  * Return: the number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
