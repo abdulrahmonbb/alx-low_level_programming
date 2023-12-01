c333d56b177575ac32d85b7b02e240ba4ec6916d
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-12-01 08:30:10 -0800

    reimplementing a cp program

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,23 +16,30 @@ int main(int argc, char *argv[])
 
 	src_fd = open(argv[1], O_RDONLY);
 	if (src_fd == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
-	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (dest_fd == -1)
+	{
 		dprintf(2, "Error: Can't write to file %s\n", argv[2]), exit(99);
-
+	}
 	while (num1 == 1024)
 	{
 		num1 = read(src_fd, buffer, 1024);
 		if (num1 == -1)
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		num2 = write(dest_fd, buffer, num1);
 		if (num2 < num1)
 			dprintf(2, "Error: Can't write to file %s\n", argv[2]), exit(99);
 	}
 	if (close(src_fd) == -1)
 		dprintf(2, "Error: Can't close fd %d\n", src_fd), exit(100);
+
 	if (close(dest_fd) == -1)
 		dprintf(2, "Error: Can't close fd %d\n", dest_fd), exit(100);
 
