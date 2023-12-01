32bf08c149bf43b198e7287c9ed2ffd9e022a1dd
Abdulrahmon Babatunde <abdulrahmonbb@gmail.com>
2023-12-01 04:05:59 -0800

    fixed errors in 2-append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,29 +1,29 @@
 #include "main.h"
 /**
- * append_text_to_file - appends text at the end of a file;
- * @filename: file string name
- * @text_content: text string to be appended
- * Return: 1 on success and -1 otherwise
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file to be appended to
+ * @text_content: text to add at the end of the file
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, res_write;
+	int fd, i = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
 	{
-		res_write = write(fd, text_content, strlen(text_content));
-		if (res_write == -1)
-		{
-			return (-1);
-			close (fd);
-		}
-		
+		i++;
 	}
-	close (fd);
+
+	write(fd, text_content, i);
 	return (1);
 }
